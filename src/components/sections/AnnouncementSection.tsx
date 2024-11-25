3b55ef46195ca392380b9accfbba848d8c3a94da
Riki <ricardocr987@gmail.com>
2024-11-25 17:55:42 +0100

    fix link

--- src/components/sections/AnnouncementSection.tsx
+++ src/components/sections/AnnouncementSection.tsx
@@ -11,17 +11,7 @@ const announcements: Announcement[] = [
   {
     title: "Subvención de la Consellería de Innovación, Industria, Comercio y Turismo 2023",
     date: "Septiembre 6, 2023",
-    url: "/article-url",
-  },
-  {
-    title: "Subvención de la Consellería de Innovación, Industria, Comercio y Turismo 2023",
-    date: "Septiembre 6, 2023",
-    url: "/article-url",
-  },
-  {
-    title: "Subvención de la Consellería de Innovación, Industria, Comercio y Turismo 2023",
-    date: "Septiembre 6, 2023",
-    url: "/article-url",
+    url: "articles/subvencion-de-la-conselleria-de-innovacion-industria-comercio-y-turismo-2023",
   },
 ];
 
@@ -31,8 +21,8 @@ export const AnnouncementSection: React.FC = () => {
       <div className="bg-gradient-to-r from-orange-600 to-orange-500 shadow-md rounded-lg p-6 w-full flex flex-col items-center text-white">
         <h2 className="text-3xl font-bold text-center text-white mb-4">Anuncios</h2>
         <div className="relative w-full">
-          <Carousel className="overflow-hidden rounded-lg" opts={{ loop: true, slidesToScroll: 3, align: 'start' }}>
-            <CarouselContent className="flex px-2">
+          <Carousel className="overflow-hidden rounded-lg">
+            <CarouselContent className="flex justify-center items-center">
               {announcements.map((announcement, index) => (
                 <CarouselItem key={index} className="bg-neutral-900 text-white hover:bg-gray-800 max-w-80 p-3 rounded-lg shadow-lg mx-2 transition duration-300">
                   <a
