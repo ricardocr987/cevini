93fe3f784a9a4968cfccd4587f941a997db9c57b
Riki <ricardocr987@gmail.com>
2024-12-16 20:57:56 +0100

    small changes

--- src/components/sections/VideoSection.tsx
+++ src/components/sections/VideoSection.tsx
@@ -17,18 +17,20 @@ export const VideoSection = () => {
 const HeroContent = () => {
     return (
         <div className="relative h-full container mx-auto px-4 text-center">
-            <div className="h-full flex flex-col space-y-12 justify-center items-center">
-                <p className="text-white text-2xl font-bold text-center max-w-[700px] leading-relaxed">
+            <div className="h-full flex flex-col space-y-12 justify-center items-center mx-28">
+                <p className="text-white text-lg md:text-xl font-bold text-center max-w-[700px] leading-relaxed">
                     Desde 1970, fabricamos ladrillos que construyen hogares con
-                    <span className="ml-2 text-3xl font-extrabold text-black">paredes de verdad</span>
+                    <span className="ml-2 text-xl md:text-2xl font-extrabold bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 bg-clip-text text-transparent">
+                        paredes de verdad
+                    </span>
                 </p>
 
                 <Image
                     alt="Cerámica Virgen de las Nieves Logo"
                     className="rounded-lg"
                     src="/media/cevini.svg"
-                    width={400}
-                    height={400}
+                    width={200}
+                    height={200}
                     priority
                 />
 
